fccabdc51c410e7e2698b6296d9fc2ced03036c2
Tom Smith <tom@sanity.io>
2025-09-26 10:50:48 +0100

    chore: update deps, enable autoUpdates, switch to pnpm

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {
-    projectId: 'uvxetc8v',
-    dataset: 'production',
-  },
-  autoUpdates: false,
+  api: {projectId: 'uvxetc8v', dataset: 'production'},
   studioHost: 'roles-workshop',
+  deployment: {appId: '53b8a5bdb041327daf2d451c', autoUpdates: true},
 })
